9c39619d169cd1602ce88e2b9376814e816337be
John Law <johnlaw.po@gmail.com>
2022-03-12 13:13:54 +0000

    chore: fix style

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use tokio::{net::UdpSocket, sync::mpsc};
 use std::{io, net::SocketAddr, sync::Arc};
+use tokio::{net::UdpSocket, sync::mpsc};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let listen = "0.0.0.0:9898";
-    let socket = UdpSocket::bind(listen.parse::<SocketAddr>().unwrap()).await?;
+    let listen_addr = "0.0.0.0:9898";
+    let socket = UdpSocket::bind(listen_addr.parse::<SocketAddr>().unwrap()).await?;
     let r = Arc::new(socket);
     let s = r.clone();
     let (tx, mut rx) = mpsc::channel::<(Vec<u8>, SocketAddr)>(1_000);
@@ -15,7 +15,7 @@ async fn main() -> io::Result<()> {
         }
     });
     let mut buf = [0; 65536];
-    println!("Listening at {}", listen);
+    println!("Listening at {}", listen_addr);
     loop {
         let (len, addr) = r.recv_from(&mut buf).await?;
         println!("{:?} bytes received from {:?}", len, addr);
