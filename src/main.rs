4813975e07cba2dafd7a3101313ff29036d431a3
John Law <johnlaw.po@gmail.com>
2022-03-12 13:04:18 +0000

    init: add basic code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::{net::UdpSocket, sync::mpsc};
+use std::{io, net::SocketAddr, sync::Arc};
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listen = "0.0.0.0:9898";
+    let socket = UdpSocket::bind(listen.parse::<SocketAddr>().unwrap()).await?;
+    let r = Arc::new(socket);
+    let s = r.clone();
+    let (tx, mut rx) = mpsc::channel::<(Vec<u8>, SocketAddr)>(1_000);
+    tokio::spawn(async move {
+        while let Some((bytes, addr)) = rx.recv().await {
+            let len = s.send_to(&bytes, &addr).await.unwrap();
+            println!("{:?} bytes echoed", len);
+        }
+    });
+    let mut buf = [0; 65536];
+    println!("Listening at {}", listen);
+    loop {
+        let (len, addr) = r.recv_from(&mut buf).await?;
+        println!("{:?} bytes received from {:?}", len, addr);
+        tx.send((buf[..len].to_vec(), addr)).await.unwrap();
+    }
 }
